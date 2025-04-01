712bcf4dc55885c69f1c4099bd6d84372e98603a
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-04-01 02:43:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -401,7 +401,6 @@ with st.container(border=True):
         fig_grafo = go.Figure(data=[edge_trace, node_trace],
                               layout=go.Layout(
                                   title=dict(text="Correlação entre Produtos do Top 10", font=dict(size=16)),
-                                  titlefont_size=16,
                                   showlegend=False,
                                   margin=dict(t=40, l=0, r=0, b=0),
                                   hovermode="closest",
